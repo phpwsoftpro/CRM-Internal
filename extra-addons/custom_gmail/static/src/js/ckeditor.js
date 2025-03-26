dd06cae41bddf742d2570e99d88e02d5491f8643
NguyenThuong <thuongnvhe140301@fpt.edu.vn>
2025-03-26 15:52:57 +0700

    update sync mail

--- extra-addons/custom_gmail/static/src/js/ckeditor.js
+++ extra-addons/custom_gmail/static/src/js/ckeditor.js
@@ -1,15 +1,33 @@
 /** @odoo-module **/
 
 export function loadCKEditor() {
-    if (!document.getElementById("ckeditor_script")) {
-        let script = document.createElement("script");
-        script.id = "ckeditor_script";
-        script.src = "https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js";
-        script.onload = () => {
-            console.log("CKEditor Loaded!");
-        };
-        document.head.appendChild(script);
-    }
+    return new Promise((resolve, reject) => {
+        if (!document.getElementById("ckeditor_script")) {
+            let script = document.createElement("script");
+            script.id = "ckeditor_script";
+            script.src = "https://cdn.ckeditor.com/ckeditor5/39.0.0/classic/ckeditor.js";
+            script.onload = () => {
+                console.log("CKEditor Loaded!");
+                resolve();
+            };
+            script.onerror = () => {
+                reject(new Error("Failed to load CKEditor"));
+            };
+            document.head.appendChild(script);
+        } else if (window.ClassicEditor) {
+            // CKEditor đã tải rồi
+            resolve();
+        } else {
+            // Nếu script đang được tải, chờ nó hoàn tất
+            const existingScript = document.getElementById("ckeditor_script");
+            existingScript.onload = () => {
+                resolve();
+            };
+            existingScript.onerror = () => {
+                reject(new Error("Failed to load CKEditor"));
+            };
+        }
+    });
 }
 
 export async function initCKEditor() {
@@ -20,7 +38,7 @@ export async function initCKEditor() {
         if (el && window.ClassicEditor) {
             ClassicEditor.create(el)
                 .then(editor => {
-                    window.editorInstance = editor; // ✅ Gán đúng
+                    this.editorInstance = editor; // ✅ Gán đúng
                 })
                 .catch(error => {
                     console.error("Error loading CKEditor:", error);
@@ -28,3 +46,4 @@ export async function initCKEditor() {
         }
     }, 100);
 }
+
