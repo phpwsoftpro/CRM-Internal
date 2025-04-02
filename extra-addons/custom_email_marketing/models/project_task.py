0b94c11749b0ad01765b7c31eba58ef3b0391164
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2025-04-02 09:07:14 +0700

    hide invoicing

--- extra-addons/custom_email_marketing/models/project_task.py
+++ extra-addons/custom_email_marketing/models/project_task.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 
 class ProjectTask(models.Model):
-    _inherit = "project.task"
+    _inherit = ['project.task', 'mail.thread']
 
     remaining_days = fields.Char(
         string="Remaining Time", compute="_compute_remaining_days"
@@ -25,7 +25,13 @@ class ProjectTask(models.Model):
         string="Connected Tasks",
     )
     cover_image = fields.Binary("Cover Image")
-
+    user_ids = fields.Many2many(
+        'res.users',
+        'custom_project_task_user_rel',  # 👈 đổi tên bảng phụ
+        'task_id',
+        'user_id',
+        string='Assigned Users'
+    )
     @api.model
     def create(self, vals):
         record = super(ProjectTask, self).create(vals)
@@ -164,57 +170,6 @@ class ProjectTask(models.Model):
             },
         }
 
-    # Notification
-    # @api.model
-    # def create(self, vals):
-    #     task = super().create(vals)
-    #     message = f'{self.env.user.name} created task "{task.name}"'
-
-    #     # 🔴 Gửi realtime
-    #     self.env["bus.bus"]._sendone(
-    #         (self.env.cr.dbname, "notification_channel", self.env.uid),
-    #         "notification",
-    #         {"message": message},
-    #     )
-
-    #     # ✅ Lưu lại
-    #     self.env["custom.notification"].create({
-    #         "user_id": self.env.uid,
-    #         "message": message,
-    #     })
-
-    #     return task
-
-    # def write(self, vals):
-    #     res = super().write(vals)
-    #     for task in self:
-    #         message = f'{self.env.user.name} updated "{task.name}"'
-    #         self.env["bus.bus"]._sendone(
-    #             (self.env.cr.dbname, "notification_channel", self.env.uid),
-    #             "notification",
-    #             {"message": message},
-    #         )
-    #         self.env["custom.notification"].create({
-    #             "user_id": self.env.uid,
-    #             "message": message,
-    #         })
-    #     return res
-
-    # def message_post(self, **kwargs):
-    #     result = super().message_post(**kwargs)
-    #     if kwargs.get("body"):
-    #         for task in self:
-    #             message = f'{self.env.user.name} commented on "{task.name}"'
-    #             self.env["bus.bus"]._sendone(
-    #                 (self.env.cr.dbname, "notification_channel", self.env.uid),
-    #                 "notification",
-    #                 {"message": message},
-    #             )
-    #             self.env["custom.notification"].create({
-    #                 "user_id": self.env.uid,
-    #                 "message": message,
-    #             })
-    #     return result
     def action_open_in_new_tab(self):
         """Open the task in a new tab using client action"""
         self.ensure_one()
@@ -223,18 +178,3 @@ class ProjectTask(models.Model):
             'url': '/web#id=%d&model=project.task&view_type=form' % self.id,
             'target': 'new',
         }
-    # @api.model
-    # def write(self, vals):
-    #     res = super().write(vals)
-    #     if 'stage_id' in vals:
-    #         for task in self:
-    #             task.env['bus.bus'].sendone(
-    #                 'task.move',
-    #                 {
-    #                     'task_id': task.id,
-    #                     'task_name': task.name,
-    #                     'new_stage': task.stage_id.name,
-    #                     'user': task.env.user.name,
-    #                 }
-    #             )
-    #     return res
