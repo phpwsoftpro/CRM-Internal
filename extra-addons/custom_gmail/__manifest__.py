654770f4fecbca5ba2be30f2fb2c249b7019fcca
lebad280324 <lebadung561@gmail.com>
2025-05-12 14:17:38 +0700

    update

--- extra-addons/custom_gmail/__manifest__.py
+++ extra-addons/custom_gmail/__manifest__.py
@@ -33,6 +33,7 @@
             "custom_gmail/static/src/js/ckeditor.js",
             "custom_gmail/static/src/js/template.js",
             "custom_gmail/static/src/js/upload_adapter.js",
+            "custom_gmail/static/src/js/kanban_header_cogmenu.js"
         ]
     },
     "installable": True,
