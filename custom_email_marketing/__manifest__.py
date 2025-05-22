d5d076b99e09507ba0b78dfeccd4d315536cff3d
DuyLap <hoangduylap124@gmail.com>
2025-05-22 12:49:52 +0700

    check conflict

--- custom_email_marketing/__manifest__.py
+++ custom_email_marketing/__manifest__.py
@@ -36,7 +36,6 @@
         "views/project_task_view.xml",
         "views/send_task_email_wizard_view.xml",
         "wizards/project_task_move_views.xml",
-        # "views/mailing_mailtext_views.xml",
         "views/mailing_list_views_inherit.xml",
     ],
     "installable": True,
